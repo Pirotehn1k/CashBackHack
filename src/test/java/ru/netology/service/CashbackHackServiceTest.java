c573d37de4dc1d7d14d00668f6d1501c7a4f18f7
Yulian Akshov <AKsh4751@gmail.com>
2023-08-25 01:01:25 +0300

    add tests

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,33 @@
+package ru.netology.service;
+
+import static org.testng.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @org.testng.annotations.Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemainLowerBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual, expected);
+    }
+
+    @org.testng.annotations.Test
+    public void testRemainHigherBoundary() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1300;
+        int actual = service.remain(amount);
+        int expected = 700;
+        assertEquals(actual, expected);
+    }
+}
